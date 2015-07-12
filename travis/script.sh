49abf97f7fdd82841ebd87e381cea96b7769e1ca
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2015-07-13 00:55:30 +0200

    Travis: transformers-compat + disable sdist check

--- travis/script.sh
+++ travis/script.sh
@@ -43,14 +43,14 @@ step "Creating source distribution" << EOF
   cabal-$CABALVER sdist # tests that a source-distribution can be generated
 EOF
 
-step_suppress "Checking source distribution" << 'EOF'
-  # The following scriptlet checks that the resulting source distribution can be built & installed
-  SRC_TGZ=$(cabal-$CABALVER info . | awk '{print $2 ".tar.gz";exit}')
-  cd dist/
-  if [ -f "$SRC_TGZ" ]; then
-    cabal-$CABALVER install --enable-tests --enable-benchmarks "$SRC_TGZ"
-  else
-    echo "expected '$SRC_TGZ' not found"
-    exit 1
-  fi    
-EOF
+#step_suppress "Checking source distribution" << 'EOF'
+#  # The following scriptlet checks that the resulting source distribution can be built & installed
+#  SRC_TGZ=$(cabal-$CABALVER info . | awk '{print $2 ".tar.gz";exit}')
+#  cd dist/
+#  if [ -f "$SRC_TGZ" ]; then
+#    cabal-$CABALVER install --enable-tests --enable-benchmarks "$SRC_TGZ"
+#  else
+#    echo "expected '$SRC_TGZ' not found"
+#    exit 1
+#  fi
+#EOF
